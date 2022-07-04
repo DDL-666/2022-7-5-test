fd3846fa307e20661c9bf4ae43d03ac54d82b56b
DDL <2670439442@qq.com>
2022-07-04 16:57:20 +0800

    米米商城项目第一次提交

---
+++ mimissm/src/main/java/com/bjpowernode/service/ProductTypeService.java
@@ -0,0 +1,10 @@
+package com.bjpowernode.service;
+
+import com.bjpowernode.pojo.ProductType;
+
+import java.util.List;
+
+public interface ProductTypeService {
+    //商品的类型
+    List<ProductType> getAll();
+}
